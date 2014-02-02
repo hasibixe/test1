da2616e5470cb5487a4ec225d615f8c67a284be8
hasibixe <hasibixe@gmail.com>
2014-02-02 17:13:41 +0800

    first commit

---
+++ dsds/ejbModule/com/appzillion/testing/TestSessionBean.java
@@ -0,0 +1,20 @@
+package com.appzillion.testing;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TestSessionBean
+ */
+@Stateless
+@LocalBean
+public class TestSessionBean {
+
+    /**
+     * Default constructor. 
+     */
+    public TestSessionBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
